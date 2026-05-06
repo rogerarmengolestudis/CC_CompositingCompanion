2db718c8103c9a6484730eedee5bf52addfb17c3
Roger Armengol <rogerarmengol.estudis@gmail.com>
2026-05-06 21:06:50 +0200

    Implement node selection and alignment functions in node_align.py

--- scripts/node_align.py
+++ scripts/node_align.py
@@ -4,3 +4,32 @@
 
 import nuke
 
+
+
+def _getSelectedNodes():
+    sel = nuke.selectedNodes()
+    if len(sel) < 2:
+        nuke.message('Please select at least 2 nodes to align.')
+        return None
+
+    return sel
+
+def _selBounds(node):
+    x = node.xpos()
+    y = node.ypos()
+    w = node.screenWidth()
+    h = node.screenHeight()
+
+    return (x, y, w, h)
+
+def alignNodes(mode="center"):
+    nodes = _getSelectedNodes()
+
+    if not nodes:
+        nuke.message('No nodes selected for alignment.')
+
+    bounds = {n: _selBounds(n) for n in nodes}
+
+# continuar aqui <-------------------------------------------------------!!!!!!!!!!!!!, fer operacions de aliniació :) 
+
+
