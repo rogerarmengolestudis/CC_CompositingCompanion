43d0faeeed282caca1a350b6023f72a5e1a551ab
Roger Armengol <rogerarmengol.estudis@gmail.com>
2026-05-06 19:16:52 +0200

    class system and first basic menus integrated

--- autoMenu/autoMenu.py
+++ autoMenu/autoMenu.py
@@ -10,16 +10,19 @@ from variables import CCVariables
 ccVars = CCVariables()
 
 # ----- Directory paths -----
-
-
-
-
+ICONS_DIR  = ccVars.ICONS_DIR
+TEMPLATES_DIR = ccVars.TEMPLATES_DIR
+GIZMOS_DIR = ccVars.GIZMOS_DIR
 
 # ///////////////////////////////////////////////////////////////////
 # Helpers
 # ///////////////////////////////////////////////////////////////////
 
+def _icon(filename):
+    """Return the absolute path for an icon file, or None if it doesn't exist."""
 
+    path = os.path.join(ICONS_DIR, filename)
+    return path if os.path.isfile(path) else None
     
 
 
@@ -44,4 +47,29 @@ class CCMenuBuilder:
         """
         Build the Compositing Companion menu.
         """
+
+        try:
+            toolbar = nuke.menu("Nodes")
+            tb_ccMenu = toolbar.addMenu("CompositingCompanion", icon = _icon("2LoudCompoBuilder.png"))
+
+            # ----- Templates submenu -----
+            self._buildTemplatesMenu(tb_ccMenu)
+            # ----- Gizmos submenu -----
+            self._buildGizmosMenu(tb_ccMenu)
+            
+        except Exception as e:
+            nuke.message("[CC] Error when building menu: {}".format(e))
+
+    def _buildTemplatesMenu(self, parentMenu):
+        """Build the "Templates" submenu"""
+
+        parentMenu.addCommand("Estofat")
+        parentMenu.addCommand("Puré")
+    
+    def _buildGizmosMenu(self, parentMenu):
+        """Build the "Gizmos" submenu"""
+
+        parentMenu.addCommand("Patata")
+        parentMenu.addCommand("Ceba")
+
 
